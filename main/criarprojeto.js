8257a008acfc772f37781e4fac326e8b2a6f10d1
cahves1 <athoschaveslopesdealmeida@gmail.com>
2025-08-28 17:59:41 -0300

    Sem ideias

---
+++ main/criarprojeto.js
@@ -0,0 +1,12 @@
+const addProjectBtn = document.getElementById('addProjectBtn')
+const projectList = document.getElementById('project-list')
+
+class Project {
+  constructor(name) {
+    this.name = name
+  }
+
+  createProject() {
+    const btn = document.createElement('button')
+  }
+}
---
+++ main/criartarefa.js
@@ -0,0 +1,2 @@
+const taskBtn = document.getElementById('addTaskBtn')
+
