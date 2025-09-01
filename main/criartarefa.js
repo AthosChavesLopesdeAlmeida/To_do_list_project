1d640310dc5f3cb2a8d07600833cd3875ca1345d
cahves1 <athoschaveslopesdealmeida@gmail.com>
2025-09-01 20:33:03 -0300

    ...

--- main/criartarefa.js
+++ main/criartarefa.js
@@ -1,24 +1,2 @@
-const addTaskBtn = document.getElementById('addTaskBtn')
-const createTaskModal = document.getElementById('createTaskModal')
-const createProjectModal = document.getElementById('createProjectModal')
+import { els } from './elementos.js'
 
-const taskNameInput = document.getElementById('taskNameInput')
-const taskDueDate = document.getElementById('taskDueDate')
-const taskProject = document.getElementById('taskProject')
-
-const taskName = document.getElementsByClassName('taskTitle')
-const taskDeadline = document.getElementById('taskDealine')
-const taskdelete = document.getElementById('taskDelete')
-
-
-addTaskBtn.addEventListener('click', function(){
-  createTaskModal.style.display = 'block'
-  createProjectModal.style.display = 'none'
-})
-
-
-
-function createTask(){
-  const newTask = document.createElement('div')
-  newTask.classList.add('task')
-}
